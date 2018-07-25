8513ee2c6bfd212c68e9fa953cb8eda27a71e29b
JagrutiDhondage <jagrutidh@gmail.com>
2018-07-25 13:08:36 +0530

    first

---
+++ js/main.js
@@ -0,0 +1,94 @@
+
+(function ($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    
+
+})(jQuery);
+
+
+// My Code
+
+function Init(){
+	var loginbtn = document.getElementById("submitBtn");
+	loginbtn.addEventListener('click',this.login.bind(this));
+};
+
+Init.prototype.login = function(){
+
+    console.log("hello");
+
+
+    var userId1 = document.getElementById("userId").value;
+    var passwd1 = document.getElementById("passwd").value;
+    console.log(userId,passwd);
+
+    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+    xmlhttp.open("POST", "/json-handler");
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    xmlhttp.send(JSON.stringify({userId:userId, passwd:passwd1}));
+    
+    xmlhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            console.log(xmlhttp.responseText);
+        }
+    }
+	
+	
+};
+
+
+
+window.onload = function(){
+	window.Init = new Init();
+};
