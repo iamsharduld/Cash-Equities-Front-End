8ba9e74f4f3889bc06588924d8fb73dae2cd8ba4
JagrutiDhondage <jagrutidh@gmail.com>
2018-07-30 10:52:33 +0530

    security pages

--- js/display_securities.js
+++ js/display_securities.js
@@ -1,43 +1,45 @@
 
 var jsonarr= [
     {
-      "id" : 1,
-    "display": "HTML Tutorial",
-    "url": "https://www.w3schools.com/html/default.asp"
+      "company" : "TITAN COMPANY LTD",
+    "sector": "FMCG",
+    "symbol": "TITAN",
+    "ISIN": "INE280A01028"
     },
     {
-      "id" : 1,
-    "display": "CSS Tutorial",
-    "url": "https://www.w3schools.com/css/default.asp"
+      "company" : "EICHER MOTORS LTD",
+    "sector": "AUTO",
+    "symbol": "EICHERMOT",
+    "ISIN": "INE066A01013"
     },
     {
-      "id" : 1,
-    "display": "JavaScript Tutorial",
-    "url": "https://www.w3schools.com/js/default.asp"
+      "company" : "BHARTI AIRTEL LTD",
+    "sector": "TELECOM",
+    "symbol": "BHARATIARTL",
+    "ISIN": "INE397D01024"
     },
     {
-      "id" : 1,
-    "display": "SQL Tutorial",
-    "url": "https://www.w3schools.com/sql/default.asp"
+      "company" : "MARUTI UDYOG LTD",
+    "sector": "FMCG",
+    "symbol": "TITAN",
+    "ISIN": "INE280A01028"
     },
     {
-      "id" : 1,
-    "display": "PHP Tutorial",
-    "url": "https://www.w3schools.com/php/default.asp"
-    },
-    {
-      "id" : 1,
-    "display": "XML Tutorial",
-    "url": "https://www.w3schools.com/xml/default.asp"
+      "company" : "CIPLA LTD",
+    "sector": "FMCG",
+    "symbol": "TITAN",
+    "ISIN": "INE280A01028"
     }
     ]
     function addRow(jsonObj) {
         var table = document.getElementById("securities");
         var row = table.insertRow(1);
-    
-        row.insertCell(0).innerHTML = jsonObj.id;
-        row.insertCell(1).innerHTML = jsonObj.display;
-        row.insertCell(2).innerHTML = jsonObj.url;
+        //row.addEventListener("click",function() {document.getElementById("para").innerHTML = jsonObj.display});
+        row.addEventListener("click",function() {popup(jsonObj)});
+        row.insertCell(0).innerHTML = jsonObj.company;
+        row.insertCell(1).innerHTML = jsonObj.sector;
+        row.insertCell(2).innerHTML = jsonObj.symbol;
+        row.insertCell(3).innerHTML = jsonObj.ISIN;
       //  document.getElementById("securities").innerHTML = jsonObj.id;
     }
     
@@ -60,4 +62,35 @@ var jsonarr= [
     function search(){
         var x = document.getElementById("mySearch").placeholder;
         //document.getElementById("demo").innerHTML = x;
+    }
+
+    function popup(jsonObj){
+        
+        var modal = document.getElementById('myModal');
+        // When the user clicks the row, open the modal 
+        modal.style.display = "block";
+        
+        //WRITE CONTENT 
+        var line1 = document.getElementById('line1');
+        var line2 = document.getElementById('line2');
+        var line3 = document.getElementById('line3');
+        var line4 = document.getElementById('line4');
+        line1.innerHTML = "Company : " +  jsonObj.company;
+        line2.innerHTML = "Sector : " +  jsonObj.sector;
+        line3.innerHTML = "Symbol : " + jsonObj.symbol;
+        line4.innerHTML = "ISIN : " + jsonObj.ISIN;
+        
+        
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+         if (event.target == modal) {
+           modal.style.display = "none";
+        }
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+    }
     }
